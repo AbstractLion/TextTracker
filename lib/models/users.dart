420626248fdb199ddf01893ce270ec51882222eb
Leon Si <leonzalion@gmail.com>
2020-05-16 13:44:01 -0400

    hi

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -7,24 +7,28 @@ class Users {
           'https://www.gravatar.com/avatar/71b976c366a90ffcefcd8f2d6535cfd2?d=https%3A%2F%2Fdevpost-challengepost.netdna-ssl.com%2Fassets%2Fdefaults%2Fno-avatar-180.png&s=180',
       handle: 'avanrocks',
       name: 'Avaneesh Kulkarni',
+      reputation: 0,
     ),
     User(
       avatarUrl:
           'https://media-exp1.licdn.com/dms/image/C4D03AQEUgiIGoubZSg/profile-displayphoto-shrink_100_100/0?e=1594857600&v=beta&t=IYQoQIDKsVhmuwRPVK6SkvSreatI2TOeCcUSTQ9y97w',
       handle: 'leonzalion',
       name: 'Leon Si',
+      reputation: 9001,
     ),
     User(
       avatarUrl:
           'https://lh6.googleusercontent.com/-_nBF0nCta-k/AAAAAAAAAAI/AAAAAAAAIsM/sGWdxvdrtqgKYUnjuvu6HDsRMppamS8BgCLcDEAE/s32-c-k-no/photo.jpg',
       handle: 'sir.melitonk',
       name: 'Kevin Wang',
+      reputation: 0,
     ),
     User(
       avatarUrl:
           'https://media-exp1.licdn.com/dms/image/C4D03AQEyzcJ9UyrGtA/profile-displayphoto-shrink_200_200/0?e=1594857600&v=beta&t=kZZPJ914NnMXhueWSv8mql-p-NBrFmhcJkVvOGhWkW0',
       handle: 'abstractultra',
       name: 'Dragon He',
+      reputation: -9001,
     ),
   ];
 

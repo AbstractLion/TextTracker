d8a49e73af6b9045edca33a6a46482f246dcc635
Leon Si <leonzalion@gmail.com>
2020-05-15 20:47:18 -0400

    hi

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
 import 'transition_route_observer.dart';
 import 'dart:math' as math;
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 
 class DashboardScreen extends StatefulWidget {
   static const routeName = '/dashboard';
@@ -66,35 +68,60 @@ class _DashboardScreenState extends State<DashboardScreen>
       onWillPop: () => _goToLogin(context),
       child: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            title: Transform.rotate(
-              angle: -math.pi,
-              child: WaveWidget(
-                config: CustomConfig(
-                  gradients: [
-                    [Colors.red, Color(0xEEF44336)],
-                    [Colors.red[800], Color(0x77E57373)],
-                    [Colors.orange, Color(0x66FF9800)],
-                    [Colors.yellow, Color(0x55FFEB3B)]
-                  ],
-                  heightPercentages: [0.25, 0.26, 0.28, 0.31],
-                  durations: [35000, 19440, 10800, 6000],
-                  blur: MaskFilter.blur(BlurStyle.solid, 10),
-                  gradientBegin: Alignment.bottomLeft,
-                  gradientEnd: Alignment.topRight,
-                ),
-                waveAmplitude: 0,
-                backgroundColor: Colors.transparent,
-                size: Size(double.infinity, 200),
-              ),
-            ),
-          ),
+          appBar: TextTrackerAppBar(),
+          bottomNavigationBar: TextTrackerBottomNavigationBar(),
           body: Container(
-              width: double.infinity,
-              height: double.infinity,
-              child: Container()),
+            width: double.infinity,
+            height: double.infinity,
+            child: Container()),
         ),
       ),
+    );
+  }
+}
+
+class TextTrackerAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -math.pi,
+      child: WaveWidget(
+        config: CustomConfig(
+          gradients: [
+            [Colors.red, Color(0xEEF44336)],
+            [Colors.red[800], Color(0x77E57373)],
+            [Colors.orange, Color(0x66FF9800)],
+            [Colors.yellow, Color(0x55FFEB3B)]
+          ],
+          heightPercentages: [0.20, 0.26, 0.28, 0.35],
+          durations: [1000000, 1000000, 1000000, 1000000],
+          blur: MaskFilter.blur(BlurStyle.solid, 20),
+          gradientBegin: Alignment.bottomLeft,
+          gradientEnd: Alignment.topRight,
+        ),
+        backgroundColor: Colors.transparent,
+        size: Size(double.infinity, 125),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(200.0);
+}
+
+class TextTrackerBottomNavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FancyBottomNavigation(
+      tabs: [
+        TabData(iconData: FontAwesomeIcons.comments, title: "Feed"),
+        TabData(iconData: FontAwesomeIcons.medal, title: "Rankings"),
+        TabData(iconData: FontAwesomeIcons.clipboard, title: "Quizzes"),
+        TabData(iconData: FontAwesomeIcons.compass, title: "Discover"),
+      ],
+      onTabChangedListener: (position) {
+        
+      },
     );
   }
 }

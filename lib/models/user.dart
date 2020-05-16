8e275398175b71c4194b32964833c727de018e0a
Leon Si <leonzalion@gmail.com>
2020-05-16 13:41:49 -0400

    hi

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,10 +2,12 @@ class User {
   final String avatarUrl;
   final String name;
   final String handle;
+  final int reputation;
 
   User({
     this.avatarUrl,
     this.name,
     this.handle,
+    this.reputation,
   });
 }

8dff50ed5d4c0c68edd0f1dd17df37ce86e42f5e
Leon Si <leonzalion@gmail.com>
2020-05-16 15:39:10 -0400

    hi

---
+++ lib/screens/add_book.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddBookScreen extends StatelessWidget {
+  static const routeName = '/add_book';
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('add a book');
+  }
+}
